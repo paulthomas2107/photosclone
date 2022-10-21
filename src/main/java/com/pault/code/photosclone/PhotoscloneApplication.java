c9a767c17fc23bb996f13aee39c274a3156c338d
Paul Thomas <paulthomas2107@gmail.com>
2022-10-21 12:25:22 +0100

    Initial

---
+++ src/main/java/com/pault/code/photosclone/PhotoscloneApplication.java
@@ -0,0 +1,14 @@
+package com.pault.code.photosclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PhotoscloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoscloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pault/code/photosclone/PhotoscloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pault.code.photosclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoscloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
